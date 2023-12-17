4c54b55dace1b1648fb30f8127abe66d66555671
Gean Victor <geanvictorduque@gmail.com>
2023-12-17 19:42:57 -0300

    feat: Add UserService

--- src/main/java/com/example/springstore/resources/UserResource.java
+++ src/main/java/com/example/springstore/resources/UserResource.java
@@ -1,17 +1,30 @@
 package com.example.springstore.resources;
 
 import com.example.springstore.entities.User;
+import com.example.springstore.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "maria", "maria@email.com", "999999", "12345");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
