ee8a52e9f9178b2ac087edfaf192a50b4242ad2f
Gean Victor <geanvictorduque@gmail.com>
2023-12-27 12:12:39 -0300

    feat: Add Payment entity

--- src/main/java/com/example/springstore/entities/Order.java
+++ src/main/java/com/example/springstore/entities/Order.java
@@ -29,7 +29,11 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-    public Order(){}
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order() {
+    }
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
@@ -72,6 +76,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
