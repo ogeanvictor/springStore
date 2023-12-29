631928095c8e7bc3a7f0b20afdde288368c77fc4
Gean Victor <geanvictorduque@gmail.com>
2023-12-29 18:36:20 -0300

    feat: Add troubleshot findById

--- src/main/java/com/example/springstore/services/UserService.java
+++ src/main/java/com/example/springstore/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.springstore.services;
 
 import com.example.springstore.entities.User;
 import com.example.springstore.repositories.UserRepository;
+import com.example.springstore.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
